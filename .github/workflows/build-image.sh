a8b401aca35784374ce56d613e39676fdffad5b3
jun <1582803+junwun@users.noreply.github.com>
2023-10-29 14:57:42 +0800

    add package

--- .github/workflows/build-image.sh
+++ .github/workflows/build-image.sh
@@ -29,7 +29,7 @@ make image ADD_LOCAL_KEY=1 PACKAGES="-dnsmasq -ip-tiny dnsmasq-full ip-full luci
     luci-i18n-statistics-zh-cn luci-i18n-watchcat-zh-cn luci-i18n-acme-zh-cn \
     luci-i18n-nft-qos-zh-cn luci-i18n-nlbwmon-zh-cn luci-i18n-adblock-zh-cn luci-i18n-dockerman-zh-cn \
     curl openssh-sftp-server lrzsz mtr tcpdump bind-tools diffutils block-mount fdisk dockerd docker-compose \
-    atop netatop dnstop httping ifstat iftop iperf3 tree whois \
+    atop netatop dnstop httping ifstat iftop iperf3 tree whois qrencode \
     collectd-mod-sensors collectd-mod-thermal acme-acmesh-dnsapi ddns-scripts-dnspod ddns-scripts-cloudflare \
     luci-i18n-passwall-zh-cn xray-core v2ray-core v2ray-geoip v2ray-geosite tcping trojan chinadns-ng brook \
     iptables-mod-conntrack-extra iptables-mod-iprange iptables-mod-socket iptables-mod-tproxy iptables-nft \
