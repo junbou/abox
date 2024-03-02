fc6222b443ea30d7be25a222444cabeb2227ec3f
jun <1582803+junwun@users.noreply.github.com>
2024-03-02 13:40:54 +0800

    use immortalwrt 23.05.1, update default settings

--- .github/workflows/build-image.sh
+++ .github/workflows/build-image.sh
@@ -5,9 +5,13 @@ release=$1
 arch=$2
 release=${release#v}
 release=${release%.*}
+release=23.05.1
+mkdir -p build
 ls -l .
-ls -l /abox
-echo "src/gz abox file:///abox" >> repositories.conf
+pushd build
+curl -L -o imagebuilder.tar.xz https://downloads.immortalwrt.org/releases/$release/targets/x86/64/immortalwrt-imagebuilder-$release-x86-64.Linux-x86_64.tar.xz
+tar -xf imagebuilder.tar.xz --strip-components=1
+echo "src/gz abox file://$GITHUB_WORKSPACE/bin/packages/$arch/abox" >> repositories.conf
 sed -i 's/CONFIG_TARGET_KERNEL_PARTSIZE=.*/CONFIG_TARGET_KERNEL_PARTSIZE=24/' .config
 sed -i 's/CONFIG_TARGET_ROOTFS_PARTSIZE=.*/CONFIG_TARGET_ROOTFS_PARTSIZE=1000/' .config
 sed -i 's/CONFIG_GRUB_EFI_IMAGES=.*/# CONFIG_GRUB_EFI_IMAGES/' .config
@@ -15,25 +19,17 @@ sed -i 's/.*CONFIG_VHDX_IMAGES.*/CONFIG_VHDX_IMAGES=y/' .config
 sed -i 's/CONFIG_USES_EXT4=.*/# CONFIG_USES_EXT4/' .config
 sed -i 's/CONFIG_TARGET_ROOTFS_TARGZ=.*/# CONFIG_TARGET_ROOTFS_TARGZ/' .config
 sed -i 's/CONFIG_TARGET_ROOTFS_EXT4FS=.*/# CONFIG_TARGET_ROOTFS_EXT4FS/' .config
-for feed in passwall_luci passwall_packages passwall2; do
-  echo "src/gz $feed https://master.dl.sourceforge.net/project/openwrt-passwall-build/releases/packages-$release/$arch/$feed" >> repositories.conf
-done
-wget https://master.dl.sourceforge.net/project/openwrt-passwall-build/passwall.pub
-fp=$(./staging_dir/host/bin/usign -F -p passwall.pub)
-mv passwall.pub keys/$fp
-fp=$(./staging_dir/host/bin/usign -F -p /abox.pub)
-cp /abox.pub keys/$fp
+fp=$(./staging_dir/host/bin/usign -F -p $GITHUB_WORKSPACE/abox.pub)
+cp $GITHUB_WORKSPACE/abox.pub keys/$fp
 make image ADD_LOCAL_KEY=1 PACKAGES="-dnsmasq -ip-tiny dnsmasq-full ip-full luci-ssl-nginx \
     luci-i18n-base-zh-cn luci-i18n-firewall-zh-cn luci-i18n-samba4-zh-cn luci-proto-wireguard \
     luci-i18n-opkg-zh-cn luci-i18n-upnp-zh-cn luci-i18n-ddns-zh-cn luci-i18n-wol-zh-cn luci-i18n-ttyd-zh-cn \
     luci-i18n-statistics-zh-cn luci-i18n-watchcat-zh-cn luci-i18n-acme-zh-cn \
-    luci-i18n-nft-qos-zh-cn luci-i18n-nlbwmon-zh-cn luci-i18n-adblock-zh-cn luci-i18n-dockerman-zh-cn \
-    curl openssh-sftp-server lrzsz mtr tcpdump bind-tools diffutils block-mount fdisk dockerd docker-compose \
-    atop netatop dnstop httping ifstat iftop iperf3 tree whois qrencode \
+    luci-i18n-nft-qos-zh-cn luci-i18n-nlbwmon-zh-cn luci-i18n-adblock-zh-cn \
+    curl openssh-sftp-server lrzsz mtr tcpdump bind-tools diffutils block-mount fdisk \
+    atop netatop dnstop httping ifstat iftop iperf3 tree whois qrencode tailscale \
     collectd-mod-sensors collectd-mod-thermal acme-acmesh-dnsapi ddns-scripts-dnspod ddns-scripts-cloudflare \
-    luci-i18n-passwall-zh-cn xray-core v2ray-core v2ray-geoip v2ray-geosite tcping trojan chinadns-ng brook \
     iptables-mod-conntrack-extra iptables-mod-iprange iptables-mod-socket iptables-mod-tproxy iptables-nft \
     ip6tables-nft ip6tables-extra kmod-ipt-nat kmod-nft-socket kmod-nft-tproxy kmod-fs-autofs4 \
-    luci-app-abox"
-pwd
-ls bin
+    luci-i18n-passwall-zh-cn luci-app-abox"
+popd
